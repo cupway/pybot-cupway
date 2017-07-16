b54f7f9b7a5aeab74329cf07c15a6b78f912a6fb
Eric Dorsey <dorseye@gmail.com>
2017-07-15 19:27:39 -0600

    add multiple conditions back for parse_command() public posts

--- pybot.py
+++ pybot.py
@@ -217,6 +217,7 @@ def parse_command(command, dm_message):
     print("@pybot in command?: {0}".format("@pybot" in command))
     print("<@U1J60L0F2> in command?: {0}".format("<@U1J60L0F2>" in command))
     print("*" * 15)
+    
 
     if dm_message == True:
         print("dm_message is True block hit!")
@@ -226,8 +227,8 @@ def parse_command(command, dm_message):
         return command
 
     # public chat room message and @pybot in message
-    #elif (dm_message == False) and ("<@U1J60L0F2>" in command == True):
-    elif dm_message == False:
+    elif dm_message == False and "<@U1J60L0F2>" in command == True:
+    #elif dm_message == False:
         print("public message and @pybot found block hit!")
         command = command.lstrip("<@U1J60L0F2> ")
         print("new command is {0}: ".format(command))
